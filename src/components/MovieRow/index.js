3db805804a164228cd64f5f79c155f3cdfb5e80f
Vinicius Mendite <vinicius.miff@gmail.com>
2020-08-26 22:34:08 -0300

    definindo modelo de listagem

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -4,17 +4,20 @@ import './styles.css';
 
 export default ({ title, items }) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow--listarea">
-        {items.results.length > 0 &&
-          items.results.map((item, index) => (
-            <img
-              key={index}
-              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-              alt={item.original_title}
-            />
-          ))}
+        <div className="movieRow--list">
+          {items.results.length > 0 &&
+            items.results.map((item, index) => (
+              <div key={index} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
