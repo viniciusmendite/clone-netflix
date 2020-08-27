c14cf290e2b6a6f4f2da96c49eb811c3948f036f
Vinicius Mendite <vinicius.miff@gmail.com>
2020-08-26 23:38:32 -0300

    criando degrade em cima do backdrop

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 import './styles.css';
 
-export default () => {
-  return <div>hello</div>;
+export default ({ item }) => {
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal" />
+      </div>
+    </section>
+  );
 };
