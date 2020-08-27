1993a48416c88ee3ddc7e4cc7c8d24d800013f7c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-08-26 22:58:46 -0300

    criando funcionalidade de busca aleatoria de filmes para exibir como backdrop

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -4,14 +4,24 @@ import './styles.css';
 import Tmdb from '../../services/Tmdb';
 
 import MovieRow from '../../components/MovieRow';
+import FeaturedMovie from '../../components/FeaturedMovie';
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
+      // buscando lista de filmes
       const list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // buscando filmes em destaque
+      const originals = list.filter((item) => item.slug === 'originals');
+      const randomChose = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      const chosen = originals[0].items.results[randomChose];
     };
 
     loadAll();
@@ -20,6 +30,7 @@ export default () => {
   return (
     <div className="page">
       <section className="lists">
+        {featuredData && <FeaturedMovie item={featuredData} />}
         {movieList.map((item, index) => (
           <MovieRow key={index} title={item.title} items={item.items} />
         ))}
