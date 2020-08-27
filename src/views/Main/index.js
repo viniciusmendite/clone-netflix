c474e3d411ce5d853a91a7d1016ba7914a67fe9c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-08-26 23:11:25 -0300

    criando api para buscar dados de uma serie especifica e salvando no estado

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -22,6 +22,8 @@ export default () => {
         Math.random() * (originals[0].items.results.length - 1)
       );
       const chosen = originals[0].items.results[randomChose];
+      const chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     };
 
     loadAll();
